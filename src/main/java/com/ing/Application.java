66079434faef613b7a70e80b7fe0c15803858d19
mbruno <mbrunopaniagua@gmail.com>
2022-06-11 14:52:09 +0000

    Initial commit

---
+++ src/main/java/com/ing/Application.java
@@ -0,0 +1,9 @@
+package com.ing;
+
+import io.micronaut.runtime.Micronaut;
+
+public class Application {
+    public static void main(String[] args) {
+        Micronaut.run(Application.class, args);
+    }
+}
